8752ec4166ffd7810514660ed1990a081afa404f
Harun Doğdu <harundogdu06@gmail.com>
2021-11-18 21:44:29 +0300

    other screens added

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>DS</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
