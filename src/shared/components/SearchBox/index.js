9af6d17591d57f34993df6556c757cb4b8d1c554
Harun Doğdu <harundogdu06@gmail.com>
2021-11-19 15:11:36 +0300

    searchbox added

---
+++ src/shared/components/SearchBox/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import themes from '../../../config/themes';
+
+const SearchBox = ({placeholder, value, onChangeText, clearText}) => {
+  return (
+    <View style={styles.container}>
+      <Feather name="search" size={20} color="#000" />
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+      />
+      {value.length > 0 && (
+        <TouchableOpacity onPress={clearText}>
+          <Feather name="x" size={20} color="#000" />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: themes.light,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+});
+
+export default SearchBox;
