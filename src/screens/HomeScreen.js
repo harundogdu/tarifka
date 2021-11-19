5da8424a1d1d9a7cafa99693f3db94d1d76a69d2
Harun Doğdu <harundogdu06@gmail.com>
2021-11-19 16:11:10 +0300

    HomeScreen Designed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,69 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {View, Text, StyleSheet, FlatList, SafeAreaView} from 'react-native';
+import themes from '../config/themes';
+import SearchBox from '../shared/components/SearchBox';
+import DATA from '../@fake_db/db.json';
+import RecipePopular from '../shared/components/RecipePopular';
+import TabRecipe from '../shared/components/TabRecipe';
+/* Function */
 const HomeScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
   return (
-    <View>
-      <Text>Sa</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.mainTitle}>Ne pişirmek istersin?</Text>
+      <View style={styles.searchContainer}>
+        <SearchBox
+          placeholder="Tarif ara..."
+          value={searchQuery}
+          clearText={() => setSearchQuery('')}
+          onChangeText={query => setSearchQuery(query)}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.mainSubTitle}>En Popülerler</Text>
+        <Text style={styles.subTitle}>Hepsini Gör</Text>
+      </View>
+      <View style={styles.recipesContainer}>
+        <RecipePopular DATA={DATA} />
+      </View>
+      <View style={styles.recipesTabContainer}>
+        <TabRecipe DATA={DATA} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  mainTitle: {
+    fontSize: 30,
+    fontWeight: '500',
+    color: themes.textColor,
+  },
+  searchContainer: {
+    marginVertical: 10,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  mainSubTitle: {
+    fontSize: 20,
+    fontWeight: '800',
+  },
+  subTitle: {
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  recipesTabContainer: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
