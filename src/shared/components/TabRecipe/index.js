cd9a619111a0b2d46b752cc061de3003b7373045
Harun Doğdu <harundogdu06@gmail.com>
2021-11-19 16:10:58 +0300

    tabRecipe added

---
+++ src/shared/components/TabRecipe/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import themes from '../../../config/themes';
+import TabCard from '../TabCard';
+
+const TabRecipe = ({DATA}) => {
+  const [selected, setSelected] = React.useState(0);
+  const tabs = ['Tüm Tarifler', 'Et Yemekleri', 'Çorbalar', 'İçecekler'];
+  return (
+    <View style={styles.container}>
+      <View style={styles.groupTitle}>
+        {tabs.map((item, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={[styles.tab, selected === index && styles.tabSelected]}
+              onPress={() => setSelected(index)}>
+              <Text
+                style={[
+                  styles.tabText,
+                  selected === index && styles.tabTextSelected,
+                ]}>
+                {item}
+              </Text>
+              {selected === index ? (
+                <View style={styles.line} />
+              ) : (
+                <View style={styles.notline} />
+              )}
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <View style={styles.innerContainer}>
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <TabCard item={item} key={item.id} />}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  groupTitle: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  innerContainer: {
+    flex: 1,
+  },
+  tabText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: themes.textColor,
+    fontWeight: 'bold',
+  },
+  tabTextSelected: {
+    color: themes.mainColor,
+  },
+  line: {
+    width: 30,
+    height: 2,
+    backgroundColor: themes.mainColor,
+    alignSelf: 'center',
+  },
+  notline: {
+    width: 30,
+    height: 2,
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+  },
+});
+
+export default TabRecipe;
