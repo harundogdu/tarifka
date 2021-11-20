9495c7525dbb43b88404264ef8a5b46d6ac86c59
Harun Doğdu <harundogdu06@gmail.com>
2021-11-20 18:20:28 +0300

    AddScreen added

---
+++ src/screens/AddScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const AddScreen = () => {
+  return (
+    <View>
+      <Text>AddScreen</Text>
+    </View>
+  );
+};
+
+export default AddScreen;
+
+const styles = StyleSheet.create({});
