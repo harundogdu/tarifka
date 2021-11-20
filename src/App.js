c3bce996a828700bcee82017650d5453974b5801
Harun Doğdu <harundogdu06@gmail.com>
2021-11-20 18:20:55 +0300

    Bottom tabs added in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 /* screens */
 import WelcomeScreen from './screens/WelcomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import HomeScreen from './screens/HomeScreen';
+import AddScreen from './screens/AddScreen';
+import TabBar from './shared/components/TabBar';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -12,9 +15,30 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+function TabNavigator() {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator
+      screenOptions={{headerShown: false}}
+      tabBar={props => <TabBar {...props} />}>
+      <Tab.Screen
+        name="Home"
+        options={{title: 'Anasayfa'}}
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        name="Add"
+        options={{title: 'Tarif Ekle'}}
+        component={AddScreen}
+      />
+    </Tab.Navigator>
   );
 }
