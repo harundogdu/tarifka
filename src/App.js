ead03b40ff5a6c747d84e0f7044d44dc9e226de3
Harun Doğdu <harundogdu06@gmail.com>
2021-11-20 20:45:48 +0300

    context added

--- src/App.js
+++ src/App.js
@@ -2,43 +2,43 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import TabBar from './shared/components/TabBar';
+import {RecipeProvider} from './shared/context/RecipeContext';
 /* screens */
 import WelcomeScreen from './screens/WelcomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import HomeScreen from './screens/HomeScreen';
 import AddScreen from './screens/AddScreen';
-import TabBar from './shared/components/TabBar';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RecipeProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Group>
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Group>         
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecipeProvider>
   );
 }
 
 function TabNavigator() {
   const Tab = createBottomTabNavigator();
-
   return (
     <Tab.Navigator
       screenOptions={{headerShown: false}}
       tabBar={props => <TabBar {...props} />}>
       <Tab.Screen
         name="Home"
-        options={{title: 'Anasayfa'}}
+        options={{title: 'Tarifler', headerShown: false}}
         component={HomeScreen}
       />
-      <Tab.Screen
-        name="Add"
-        options={{title: 'Tarif Ekle'}}
-        component={AddScreen}
-      />
+      <Tab.Screen name="Add" component={AddScreen} />
     </Tab.Navigator>
   );
 }
