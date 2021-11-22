3a614115986aa6e99a1059c2ea5132a4295efd9d
Harun Doğdu <harundogdu06@gmail.com>
2021-11-22 03:32:00 +0300

    tabbar keyboard event added

--- src/shared/components/TabBar/index.js
+++ src/shared/components/TabBar/index.js
@@ -1,85 +1,110 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Keyboard, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import themes from '../../../config/themes';
 import IonicIcons from 'react-native-vector-icons/Ionicons';
 
 function TabBar({state, descriptors, navigation}) {
+  const [keyboardState, setKeyboardState] = React.useState(true);
+
+  React.useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardState(false);
+      },
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardState(true);
+      },
+    );
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        backgroundColor: themes.mainColor,
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      {state.routes.map((route, index) => {
-        const {options} = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+    keyboardState && (
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: themes.mainColor,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {state.routes.map((route, index) => {
+          const {options} = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
 
-        const isFocused = state.index === index;
+          const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-          });
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+            });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
 
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityStates={isFocused ? ['selected'] : []}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{flex: 1, alignItems: 'center'}}
-            key={index}>
-            {route.name === 'Home' && (
-              <>
-                <IonicIcons
-                  name={isFocused ? 'ios-home' : 'ios-home-outline'}
-                  size={22}
-                  color={isFocused ? themes.white : themes.light}
-                />
-                <Text style={{color: isFocused ? themes.white : themes.light}}>
-                  {label}
-                </Text>
-              </>
-            )}
-            {route.name === 'Add' && (
-              <>
-                <IonicIcons
-                  name={isFocused ? 'share' : 'share-outline'}
-                  size={22}
-                  color={themes.white}
-                />
-                <Text style={{color: isFocused ? themes.white : themes.light}}>
-                  {label}
-                </Text>
-              </>
-            )}
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+          return (
+            <TouchableOpacity
+              accessibilityRole="button"
+              accessibilityStates={isFocused ? ['selected'] : []}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{flex: 1, alignItems: 'center'}}
+              key={index}>
+              {route.name === 'Home' && (
+                <>
+                  <IonicIcons
+                    name={isFocused ? 'ios-home' : 'ios-home-outline'}
+                    size={22}
+                    color={isFocused ? themes.white : themes.light}
+                  />
+                  <Text
+                    style={{color: isFocused ? themes.white : themes.light}}>
+                    {label}
+                  </Text>
+                </>
+              )}
+              {route.name === 'Add' && (
+                <>
+                  <IonicIcons
+                    name={isFocused ? 'share' : 'share-outline'}
+                    size={22}
+                    color={themes.white}
+                  />
+                  <Text
+                    style={{color: isFocused ? themes.white : themes.light}}>
+                    {label}
+                  </Text>
+                </>
+              )}
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    )
   );
 }
 
