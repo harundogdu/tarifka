d33ea2b4f10e6ba9e88670577273821284ba725b
Harun Doğdu <harundogdu06@gmail.com>
2021-11-23 16:25:29 +0300

    async storage used

--- src/shared/context/RecipeContext.js
+++ src/shared/context/RecipeContext.js
@@ -1,14 +1,45 @@
-import React, {createContext, useContext, useState} from 'react';
-import DATA from '../../@fake_db/db.json';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const RecipeContext = createContext();
 
 export const RecipeProvider = ({children}) => {
-  const [recipes, setRecipes] = useState(DATA);
+  const [recipes, setRecipes] = useState([]);
+
+  const storeData = async () => {
+    try {
+      const jsonValue = JSON.stringify(recipes);
+      await AsyncStorage.setItem('@recipes', jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@recipes');
+      if (jsonValue !== null) {
+        const value = JSON.parse(jsonValue);
+        setRecipes(value);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    storeData();
+  }, [recipes]);
 
   const contextValues = {
     recipes,
     setRecipes,
+    storeData,
+    getData
   };
   return (
     <RecipeContext.Provider value={contextValues}>
